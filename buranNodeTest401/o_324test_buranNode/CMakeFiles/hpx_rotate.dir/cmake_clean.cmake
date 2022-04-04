db353e5262552daf0a820158e548954ebd248307
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2022-04-03 23:01:11 -0500

    Add files via upload
    original rotate test

---
+++ buranNodeTest401/o_324test_buranNode/CMakeFiles/hpx_rotate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hpx_rotate.dir/hpx_rotate.cpp.o"
+  "CMakeFiles/hpx_rotate.dir/hpx_rotate.cpp.o.d"
+  "hpx_rotate"
+  "hpx_rotate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hpx_rotate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
